bc1c66901b9a8a5da88c7b8ff40519765140dd4e
yarin Shapira <yariniii19@gmail.com>
2020-11-17 10:00:57 +0200

    Initial commit

---
+++ Assets/Scripts/1-movers/Oscillator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ *  this component moves its object right and left or up and down in dynamic speed.
+ */
+
+public class Oscillator : MonoBehaviour {
+    [SerializeField]
+    float Speed = 1f;
+    [Tooltip("Defines the distance that the object will move from the starting point to each direction")]
+    [SerializeField]
+    float MaximumDistance = 1f;
+    [Tooltip("if check the object move up and down")]
+    [SerializeField]
+    bool upAndDown = false;
+
+
+    Vector3 startPos;
+    private float x = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float tSpeed = Speed/100; //  Decreases the number speed
+        x += tSpeed;
+        if (!upAndDown) transform.position = startPos + new Vector3(Mathf.Sin(x) * MaximumDistance, 0f, 0f);
+        else transform.position = startPos + new Vector3(0f, Mathf.Sin(x) * MaximumDistance, 0);
+
+    }
+
+}
+
+
---
+++ Assets/Scripts/HeartSystem.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * This component spawns the given object at random time-intervals.
+ */
+
+public class HeartSystem : MonoBehaviour {
+    [SerializeField] protected GameObject Heart;
+    [SerializeField] protected uint MaxNumberOfHeart = 5;
+    [SerializeField] protected uint NumberOfHeartAtStart = 3;
+    [SerializeField] protected float SpacesBetweenHearts = 1.3f;
+    [SerializeField] protected Vector3 PositionOfTheFirstHeart;
+    [SerializeField] string DamageTriggeringTag;
+    [SerializeField] string HealingTriggeringTag;
+
+
+    private int NumberOfHeart = 0;
+    private  GameObject[] Hearts;
+    // Start is called before the first frame update
+    void Start() {
+        Hearts = new GameObject[MaxNumberOfHeart];
+        for (int i = 0; i < NumberOfHeartAtStart; i++) {
+            Vector3 positionOfSpawnedObject = new Vector3(PositionOfTheFirstHeart.x + (NumberOfHeart* SpacesBetweenHearts), PositionOfTheFirstHeart.y, PositionOfTheFirstHeart.z);
+            Quaternion rotationOfSpawnedObject = Quaternion.identity;  // no rotation.
+            GameObject newObject = Instantiate(Heart, positionOfSpawnedObject, rotationOfSpawnedObject);
+            Hearts[NumberOfHeart] = newObject;
+            NumberOfHeart++;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == DamageTriggeringTag) Damage(other);
+        if (other.tag == HealingTriggeringTag) Heal(other);
+    }
+
+    private void Damage(Collider2D other) {
+        Destroy(Hearts[NumberOfHeart - 1]);
+        NumberOfHeart--;
+        Destroy(other.gameObject);
+        if (NumberOfHeart == 0) Destroy(this.gameObject);
+    }
+
+
+    public int GetNumberOfHeart() {
+        return NumberOfHeart;
+    }
+    private void Heal(Collider2D other)
+    {
+        if (NumberOfHeart < MaxNumberOfHeart)
+        {
+            Destroy(other.gameObject);
+            Vector3 positionOfSpawnedObject = new Vector3(PositionOfTheFirstHeart.x + (NumberOfHeart * SpacesBetweenHearts), PositionOfTheFirstHeart.y, PositionOfTheFirstHeart.z);
+            Quaternion rotationOfSpawnedObject = Quaternion.identity;  // no rotation.
+            GameObject newObject = Instantiate(Heart, positionOfSpawnedObject, rotationOfSpawnedObject);
+            Hearts[NumberOfHeart] = newObject;
+            NumberOfHeart++;
+        }
+
+    }
+}
